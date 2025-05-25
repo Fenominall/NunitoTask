a540c7cb084e76f8b7f976a710257eadd1bd19d3
Vladislav Todorov <it.vtodorov@gmail.com>
2025-05-25 09:25:17 +0300

    chore[app] Created new XCode project for NunitoTask
    - Added custom fonts for Nunito Sans

---
+++ NunitoTask/NunitoTask/NunitoTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  NunitoTaskApp.swift
+//  NunitoTask
+//
+//  Created by Fenominall on 5/25/25.
+//
+
+import SwiftUI
+
+@main
+struct NunitoTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
