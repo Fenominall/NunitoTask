0a5e95f81de86e04853db09901c9b49ae27e9cd9
Vladislav Todorov <it.vtodorov@gmail.com>
2025-05-25 12:51:18 +0300

    chore[app]: Add NunitoAppImages enum for safe image assets access

---
+++ NunitoTask/NunitoTask/Resources/NunitoAppImages.swift
@@ -0,0 +1,38 @@
+//
+//  NunitoAppImages.swift
+//  NunitoTask
+//
+//  Created by Fenominall on 5/25/25.
+//
+
+import SwiftUI
+
+enum AppImages {
+    case success
+    case failure
+    case noWifi
+    case cross
+    case users
+    case user
+    case scatchyCat
+    
+    var image: UIImage? {
+        switch self {
+            
+        case .success:
+            return UIImage(named: "check")
+        case .failure:
+            return UIImage(named: "failure")
+        case .noWifi:
+            return UIImage(named: "noWifi")
+        case .cross:
+            return UIImage(named: "cross")
+        case .users:
+            return UIImage(named: "users")
+        case .user:
+            return UIImage(named: "user")
+        case .scatchyCat:
+            return UIImage(named: "sketchyCat")
+        }
+    }
+}
