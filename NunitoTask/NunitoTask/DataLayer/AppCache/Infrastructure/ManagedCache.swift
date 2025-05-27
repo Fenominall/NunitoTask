bfe4c6e4e271bf20ac9032aed43055f772e6ab81
Vladislav Todorov <it.vtodorov@gmail.com>
2025-05-27 09:40:34 +0300

    feat[data-layer]: Added fetching methods to the 'ManagedCache' to fetch all users or create new context if its not yet created

--- NunitoTask/NunitoTask/DataLayer/AppCache/Infrastructure/ManagedCache.swift
+++ NunitoTask/NunitoTask/DataLayer/AppCache/Infrastructure/ManagedCache.swift
@@ -20,3 +20,24 @@ extension ManagedCache {
         (feed.array as? [ManagedUser])?.compactMap { $0.local } ?? []
     }
 }
+
+// MARK: - Fetching Cache
+extension ManagedCache {
+    static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
+        guard let entityName = entity().name else { return nil }
+        let request = NSFetchRequest<ManagedCache>(entityName: entityName)
+        request.returnsObjectsAsFaults = false
+        return try context.fetch(request).first
+    }
+    
+    private static func fetchOrCreateCache(
+        in context: NSManagedObjectContext
+    ) throws -> ManagedCache {
+        guard let oldCache = try ManagedCache.find(in: context) else {
+            let newCache = ManagedCache(context: context)
+            try context.save()
+            return newCache
+        }
+        return oldCache
+    }
+}
